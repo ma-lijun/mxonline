1493c4257e1d05e889079898bf607270722ef08c
ma-lijun <343190282@qq.com>
2017-04-25 13:27:22 +0800

    配好数据库，增加几个模块，但是有错误

--- organizations/models.py
+++ organizations/models.py
@@ -1,13 +1,51 @@
 # _*_ encording:utf-8 _*_
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 
 # Create your models here.
+class CityDict(models.Model):
+    name=models.CharField(max_length=20,verbose_name=u'城市名称')
+    desc=models.CharField(max_length=100,verbose_name=u'描述')
+    add_time=models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name=u'城市'
+        verbose_name_plural=verbose_name
+
 class CourseOrg(models.Model):
-    name=models.FileField()
-    desc=models.TextField()
-    click_nums=models.IntegerField()
-    fav_nums=models.IntegerField()
-    add_time=models.TimeField()
-    
+    name=models.CharField(max_length=50,verbose_name=u'机构名称')
+    desc=models.TextField(verbose_name=u'机构描述')
+    click_nums=models.IntegerField(default=0,verbose_name=u'点击次数')
+    fav_nums=models.IntegerField(default=0,verbose_name=u'收藏人数')
+    add_time=models.TimeField(default=datetime.now)
+    address=models.CharField(max_length=150,verbose_name=u'机构地址')
+    city=models.ForeignKey(CityDict,verbose_name=u'外键说明')
+
+    class Meta:
+        verbose_name=u'机构名称'
+        verbose_name_plural=verbose_name
+
+
+class Teacher(models.Model):
+    org=models.ForeignKey(CourseOrg,verbose_name=u"所属机构")
+    name=models.CharField(max_length=50,verbose_name=u'教师名字')
+    work_years=models.IntegerField(default=0,verbose_name=u'工作年限')
+    work_company=models.CharField(max_length=50,verbose_name=u'就职公司')
+    work_position=models.CharField(max_length=50,verbose_name=u'工作职位')
+    points=models.CharField(max_length=20,verbose_name=u'教学特点')
+    click_nums = models.IntegerField(default=0, verbose_name=u'点击次数')
+    fav_nums = models.IntegerField(default=0, verbose_name=u'收藏人数')
+    add_time = models.TimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name=u'机构名称'
+        verbose_name_plural=verbose_name
+
+
+
+
+
+
+
