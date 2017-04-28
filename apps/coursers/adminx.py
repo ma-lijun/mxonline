4bc3c9bb6c90096e11abf9927176ea542f1dc91d
ma-lijun <343190282@qq.com>
2017-04-28 18:45:31 +0800

    成功配置了两个课程模块，可以增加修改，查看 搜索了

---
+++ apps/coursers/adminx.py
@@ -0,0 +1,34 @@
+# _*_ coding:utf-8 _*_ 
+_author_ = 'mlj'
+_date_ = '2017/4/28 15:59'
+import xadmin
+
+from .models import Course,Lesson,Video,CourseResource
+
+class CourseAdmin(object):
+    list_display=['name','desc','detail','degree','learn_times','students','fav_nums','fav_nums','image','click_nums','add_time']
+    search_fields=['name','desc','detail','degree','learn_times','students','fav_nums','fav_nums','image','click_nums','add_time']
+    list_filter=['name','desc','detail','degree','learn_times','students','fav_nums','fav_nums','image','click_nums','add_time']
+
+
+class LessonAdmin(object):
+    list_display=['courser','name','add_time']
+    search_fields=['courser','name','add_time']
+    list_filter=['courser__name','name','add_time']
+
+
+class VideoAdmin(object):
+    list_display=['name','add_time']
+    search_fields=['name','add_time']
+    list_filter=['courser__name','name','add_time']
+
+
+class CourseResourceAdmin(object):
+    list_display=['course','name','add_time']
+    search_fields=['name','download']
+    list_filter=['course__name','name','add_time']
+
+xadmin.site.register(Course,CourseAdmin)
+xadmin.site.register(Lesson,LessonAdmin)
+xadmin.site.register(Video,VideoAdmin)
+xadmin.site.register(CourseResource,CourseResourceAdmin)
